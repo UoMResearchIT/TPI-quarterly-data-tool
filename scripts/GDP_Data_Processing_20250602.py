f604b9f2700e3ae3e994cefa28a8175843d5b404
McKeoghN <mckeoghn@gmail.com>
2025-02-14 14:49:01 +0000

    GDPPH calculated

--- scripts/GDP_Data_Processing_20250602.py
+++ scripts/GDP_Data_Processing_20250602.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from Quarterly_Data_Processing_20250701 import EU_format
+import numpy as np
 
 # Process UK GDP data
 UK_GDP = pd.read_csv("../src/UK GDP at market prices.csv")
@@ -29,7 +30,6 @@ PPP = PPP.rename(columns={"TIME_PERIOD": "Quarter", "Country": "geo"})
 PPP = PPP.sort_values(by="Quarter", ascending=True)
 PPP = EU_format(PPP, "PPP")
 PPP = PPP.rename(columns={"Quarter": "Year"})
-print(PPP)
 
 GDP["Year"] = GDP["Quarter"].str.extract(r"(\d{4})").astype(int)
 GDP = GDP.merge(PPP, on="Year")
@@ -38,9 +38,7 @@ print(GDP.dtypes)
 GDP["UK GDP USD"] = round(GDP["UK GDP"] * GDP["United Kingdom PPP"], 2)
 GDP["Germany GDP USD"] = round(GDP["Germany GDP"] * GDP["Germany PPP"], 2)
 GDP["France GDP USD"] = round(GDP["France GDP"] * GDP["France PPP"], 2)
-print(GDP)
-GDP_USD = GDP[["UK GDP USD", "US GDP USD", "Germany GDP USD", "France GDP USD"]]
-print(GDP_USD)
+GDP_USD = GDP[["Quarter", "UK GDP USD", "US GDP USD", "Germany GDP USD", "France GDP USD"]]
 
 # Hours worked!
 # UK data is already quarterly, hours per week - so need to times by 13 to get quarterly
@@ -50,8 +48,32 @@ print(GDP_USD)
 EU_Hours = pd.read_csv("../src/EU Hours worked.csv")
 EU_Hours = EU_Hours[["TIME_PERIOD", "geo", "OBS_VALUE"]]
 EU_Hours = EU_Hours.rename(columns={"TIME_PERIOD": "Quarter"})
-EU_Hours = EU_format(EU_Hours, "hours worked")
-print(EU_Hours)
+EU_Hours = EU_format(EU_Hours, "hours worked").dropna()
+for column in EU_Hours.columns:
+    if column != "Quarter":
+        EU_Hours[column] = EU_Hours[column] / 10**6
 
 UK_Hours = pd.read_excel("../src/ONS GVA and hours worked.xlsx", sheet_name="Table_18", usecols="A,B", skiprows=6, names=["Quarter", "Value"])
-print(UK_Hours)
+UK_Hours["Hours worked"] = UK_Hours["Value"] *13/(10**9)
+
+# Import data and format correctly
+US_Hours = pd.read_excel("../src/US hours worked.xlsx", skiprows=2, nrows=1)
+US_Hours = US_Hours.drop(["Sector", "Basis", "Component", "Units"], axis=1)
+US_Hours = US_Hours.set_index(US_Hours.columns[0]).T
+US_Hours.columns.name = None
+US_Hours = US_Hours  # Rename the first column
+US_Hours = (
+    US_Hours.reset_index()
+    .rename(columns={"index": "Quarter"})
+    .replace("N.A.", np.nan)
+    .dropna())
+# For some reason US data is always stored as an object, so you have to convert to numeric
+US_Hours["Hours worked"] = pd.to_numeric(US_Hours["Hours worked"], errors='coerce')
+
+GDPPH = GDP_USD.copy()
+GDPPH["US GDP USD"] = GDPPH["US GDP USD"] / US_Hours["Hours worked"]
+GDPPH["UK GDP USD"] = GDPPH["UK GDP USD"] / UK_Hours["Hours worked"]
+GDPPH["Germany GDP USD"] = GDPPH["Germany GDP USD"] / EU_Hours["Germany hours worked"]
+GDPPH["France GDP USD"] = GDPPH["France GDP USD"] / EU_Hours["France hours worked"]
+GDPPH = GDPPH.dropna().round(2)
+print(GDPPH)
